20c24fcb39c71c9ed3c2df4d7b7dbbfd43c903fa
Jack Baron <jackmbaron@gmail.com>
2022-04-25 17:21:20 +0100

    setup bot

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,3 +6,7 @@ const NODE_ENV = registerString('NODE_ENV')
 const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
 export const IS_DEV = !IS_PROD
 // #endregion
+
+// #region Application
+export const TOKEN = registerString('TOKEN', true)
+// #endregion
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export {}
+import { Client, Intents } from 'discord.js'
+import { TOKEN } from './env/index.js'
+import { exitHook } from './exitHook.js'
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}`)
+})
+
+void client.login(TOKEN)
+exitHook((exit, error) => {
+  console.log('Exiting...')
+  if (error instanceof Error) {
+    console.error(error)
+  }
+
+  client.destroy()
+  exit()
+})
