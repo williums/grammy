d0febb918581cb60b07ca88d87f2ddb78b19faca
Jack Baron <jackmbaron@gmail.com>
2022-04-25 17:57:13 +0100

    skip download if already saved

--- src/download.ts
+++ src/download.ts
@@ -1,5 +1,6 @@
 import mkdirp from 'mkdirp'
-import { createWriteStream } from 'node:fs'
+import { createWriteStream, type PathLike } from 'node:fs'
+import { rename, stat } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pipeline } from 'node:stream/promises'
 import ytdl from 'yt-dlp-exec'
@@ -11,10 +12,32 @@ const exec = ytdl.exec as typeof import('yt-dlp-exec')['exec']
 export const downloadClip = async (url: string) => {
   const { id, title, ext } = await ytdl(url, { dumpSingleJson: true })
   const filename = `${title} [${id}].${ext}`
+  const temporaryFilename = `${filename}.tmp`
 
   const dir = DOWNLOAD_DIR
   await mkdirp(dir)
 
+  const temporaryPath = join(dir, temporaryFilename)
   const path = join(dir, filename)
-  await pipeline(exec(url, { output: '-' }).stdout!, createWriteStream(path))
+
+  const fileExists = await exists(path)
+  if (fileExists) {
+    return
+  }
+
+  await pipeline(
+    exec(url, { output: '-' }).stdout!,
+    createWriteStream(temporaryPath)
+  )
+
+  await rename(temporaryPath, path)
+}
+
+const exists: (path: PathLike) => Promise<boolean> = async path => {
+  try {
+    await stat(path)
+    return true
+  } catch {
+    return false
+  }
 }
