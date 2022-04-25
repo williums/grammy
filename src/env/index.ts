033a14477c26c83de13063909f865a7a0b6706bd
Jack Baron <jackmbaron@gmail.com>
2022-04-25 17:14:09 +0100

    setup env helper

---
+++ src/env/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { registerString } from './register.js'
+
+// #region Globals
+const NODE_ENV = registerString('NODE_ENV')
+const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
+export const IS_DEV = !IS_PROD
+// #endregion
