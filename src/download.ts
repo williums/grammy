32eab70193c3dec2abeb39c56d3f8b3baadd1377
Brandon Williams <bwei.williams@gmail.com>
2022-10-21 12:18:08 -0600

    fix local path for cookies file

--- src/download.ts
+++ src/download.ts
@@ -4,15 +4,16 @@ import { rename, stat } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pipeline } from 'node:stream/promises'
 import ytdl from 'yt-dlp-exec'
-import { DOWNLOAD_DIR } from './env/index.js'
+import { COOKIES, DOWNLOAD_DIR } from './env/index.js'
 
 // @ts-expect-error Incorrect typings
 const exec = ytdl.exec as typeof import('yt-dlp-exec')['exec']
 
 export const downloadClip = async (url: string): Promise<string> => {
+  const cookies = `../${COOKIES}`
   const { id, ext } = await ytdl(url, {
     dumpSingleJson: true,
-    cookies: 'C:/Users/williums/Code/grammy/instagram_cookies.txt',
+    cookies: cookies,
   })
   const filename = `${id}.${ext}`
   const temporaryFilename = `${filename}.tmp`
